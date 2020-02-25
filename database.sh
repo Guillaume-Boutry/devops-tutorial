078ae76621800a7307bd11c406944c87c248d012
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-02-25 11:04:13 +0100

    Docker compose with real init sql
    Signed-off-by: Guillaume Boutry <boutryguillaume1@gmail.com>

--- database.sh
+++ database.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-SQL_FILE=$(pwd)/target/randjfiudsjfijsd.sql
 
 docker pull mariadb/server:10.4
-{ echo "DROP DATABASE IF EXISTS APPLICATION;" ;
-  echo "DROP USER IF EXISTS 'devops'@'%';" ;
-  echo "FLUSH PRIVILEGES;" ;
-  echo "CREATE DATABASE APPLICATION;" ;
-  echo "CREATE USER 'devops'@'%' IDENTIFIED BY 'devops';" ;
-  echo "GRANT ALL PRIVILEGES ON APPLICATION.* TO 'devops'@'%';" ; } > "$SQL_FILE"
 docker run --name database --rm  -p 3306:3306 \
-  -v $SQL_FILE:/docker-entrypoint-initdb.d/init.sql \
+  -v ./init_database.sql:/docker-entrypoint-initdb.d/init.sql \
   -e MARIADB_ROOT_PASSWORD=root \
   -e MARIADB_DATABASE=APPLICATION \
   -e MARIADB_USER=devops \
   -e MARIADB_PASSWORD=devops \
   -d mariadb/server:10.4
-
-# -v $HOME/.data/mariadb:/var/lib/mysql
