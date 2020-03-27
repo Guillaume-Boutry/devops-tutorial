88c050003c59dd9f752ebdf4b392020c4bcf0a64
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-03-27 02:29:58 +0100

    Correct typo
    Signed-off-by: Guillaume Boutry <boutryguillaume1@gmail.com>

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -11,6 +11,6 @@ if [ -z "$DOCKER_HOST" ]
 then
   docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$WORKSPACE":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean install
 else
-  docker run --rm -e DOCKER_HOST="$DOCKER_HOST"-v "$WORKSPACE":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean install
+  docker run --rm -e DOCKER_HOST="$DOCKER_HOST" -v "$WORKSPACE":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean install
 fi
 docker build -f src/main/docker/Dockerfile.jvm -t boutry/devops-tutorial-jvm "$WORKSPACE"
