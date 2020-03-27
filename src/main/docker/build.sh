45f4019ba9e6c85b4384bce45a3375420a03d54e
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-03-27 16:02:14 +0100

    Added docker socket
    Signed-off-by: Guillaume Boutry <boutryguillaume1@gmail.com>

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -11,6 +11,6 @@ if [ -z "$DOCKER_HOST" ]
 then
   docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$WORKSPACE":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean install
 else
-  docker run --rm --network=host -e DOCKER_HOST="$DOCKER_HOST" -e DOCKER_CERT_PATH="/certs/client" -e DOCKER_TLS_VERIFY=1 -v "$DOCKER_CERT_PATH":"$DOCKER_CERT_PATH" -v "$WORKSPACE":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean install
+  docker run --rm --network="host" -e DOCKER_HOST="$DOCKER_HOST" -e DOCKER_CERT_PATH="/certs/client" -e DOCKER_TLS_VERIFY=1 -v /var/run/docker.sock:/var/run/docker.sock -v "$DOCKER_CERT_PATH":"$DOCKER_CERT_PATH" -v "$WORKSPACE":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean install
 fi
 docker build -f src/main/docker/Dockerfile.jvm -t boutry/devops-tutorial-jvm "$WORKSPACE"
