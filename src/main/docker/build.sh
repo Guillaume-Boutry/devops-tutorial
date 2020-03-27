16df3578b625f12cf2b108c955444a8dce043f01
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-03-27 02:07:17 +0100

    Added workspace compatibility for Jenkins
    Signed-off-by: Guillaume Boutry <boutryguillaume1@gmail.com>

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# To be ran on projet root
+set -x
+# To be ran on project root
+if [ -z "$WORKSPACE" ]
+then
+  WORKSPACE=$(pwd)
+fi
+
 docker volume create --name maven-repo
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean package
-docker build -f src/main/docker/Dockerfile.jvm -t boutry/devops-tutorial-jvm "$(pwd)"
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v "$WORKSPACE":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean package
+docker build -f src/main/docker/Dockerfile.jvm -t boutry/devops-tutorial-jvm "$WORKSPACE"
