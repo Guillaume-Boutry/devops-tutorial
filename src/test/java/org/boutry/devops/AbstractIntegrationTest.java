873d22d9637d2e7cf47aecaa434d0bb2563e3452
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-03-27 11:00:23 +0100

    Remove forced ports
    Signed-off-by: Guillaume Boutry <boutryguillaume1@gmail.com>

--- src/test/java/org/boutry/devops/AbstractIntegrationTest.java
+++ src/test/java/org/boutry/devops/AbstractIntegrationTest.java
@@ -1,12 +1,10 @@
 package org.boutry.devops;
 
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MariaDBContainer;
 
 public abstract class AbstractIntegrationTest {
 
-    public static GenericContainer db = new MariaDBContainer("mariadb/server:10.4")
-            .withExposedPorts(3306, 3306);
+    public static MariaDBContainer db = new MariaDBContainer("mariadb:10.5");
 
     static {
         db.start();
