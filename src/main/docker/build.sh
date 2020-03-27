cc48afa4d23f8dccc5ba87f2ff32d2e0aef9bf94
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-03-27 02:13:15 +0100

    remove interactive mode
    Signed-off-by: Guillaume Boutry <boutryguillaume1@gmail.com>

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -7,5 +7,5 @@ then
 fi
 
 docker volume create --name maven-repo
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v "$WORKSPACE":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean package
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$WORKSPACE":/tmp/project -v maven-repo:/root/.m2 -w /tmp/project maven:3.6.3-jdk-11 mvn clean package
 docker build -f src/main/docker/Dockerfile.jvm -t boutry/devops-tutorial-jvm "$WORKSPACE"
