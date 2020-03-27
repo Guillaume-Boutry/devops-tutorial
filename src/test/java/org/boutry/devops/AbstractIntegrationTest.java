f4c0cfc11b0aa981b8f105b10e6a29776f9c332d
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-03-27 10:48:41 +0100

    force exposed port to 3306
    Signed-off-by: Guillaume Boutry <boutryguillaume1@gmail.com>

--- src/test/java/org/boutry/devops/AbstractIntegrationTest.java
+++ src/test/java/org/boutry/devops/AbstractIntegrationTest.java
@@ -1,10 +1,12 @@
 package org.boutry.devops;
 
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MariaDBContainer;
 
 public abstract class AbstractIntegrationTest {
 
-    public static MariaDBContainer db = new MariaDBContainer("mariadb/server:10.4");
+    public static GenericContainer db = new MariaDBContainer("mariadb/server:10.4")
+            .withExposedPorts(3306, 3306);
 
     static {
         db.start();
