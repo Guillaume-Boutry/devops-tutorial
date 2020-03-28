48d5cf410a036e5f42341f1c7e8793f70ae3205b
Guillaume Boutry <boutryguillaume1@gmail.com>
2020-03-28 13:09:20 +0100

    Remove CI test
    Signed-off-by: Guillaume Boutry <boutryguillaume1@gmail.com>

--- jenkins/buildDocker.sh
+++ jenkins/buildDocker.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-if [ "$CI" == "true" ]
-then
-  docker build -f src/main/docker/Dockerfile.jvm -t registry.zouzland.com/boutry/devops-tutorial-jvm .
-fi
+docker build -f src/main/docker/Dockerfile.jvm -t registry.zouzland.com/boutry/devops-tutorial-jvm .
